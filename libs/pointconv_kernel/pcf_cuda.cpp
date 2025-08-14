32e265e821adc2bab1d796995300f32b096ea2a0
Fuxin Li <fuxin.li@oregonstate.edu>
2025-08-14 15:32:33 -0700

    Added PointConv fused CUDA kernels

---
+++ libs/pointconv_kernel/pcf_cuda.cpp
@@ -0,0 +1,17 @@
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+// Copyright (C) 2025 DMV Lab
+//
+
+#include <torch/extension.h>
+#include "pcf.h"
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("pconv_forward", &pcf::pconv_forward, "PointConv forward (CUDA)");
+    m.def("pconv_linear_forward", &pcf::pconv_linear_forward, "Fused PointConv+Linear forward (CUDA)");
+    m.def("pconv_backward", &pcf::pconv_backward, "PointConv backward (CUDA)");
+    m.def("pconv_linear_backward", &pcf::pconv_linear_backward, "Fused PointConv+Linear backward (CUDA)");
+    m.def("pconv_linear_opt_backward", &pcf::pconv_linear_opt_backward, "Optimized Fused PointConv+Linear backward (CUDA)");
+    m.def("compute_knn_inverse", &pcf::compute_knn_inverse, "Compute KNN inverse mapping (CUDA)");
+    m.def("pconv_linear_cutlass_forward", &pcf::pconv_linear_cutlass, "PConv Linear forward (CUTLASS)");
+}
---
+++ libs/pointconv_kernel/setup.py
@@ -0,0 +1,38 @@
+# PCF CUDA Kernel:
+# For licensing see accompanying LICENSE file.
+# Copyright (C) 2022-2023 Apple Inc. All Rights Reserved.
+#
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import os
+
+# Get the absolute path to the project root directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, '../..'))
+
+src_files = [
+    'pcf_cuda.cpp',
+    'src/pcf.cu',
+    'src/common.cu',
+    'src/knn.cu',
+    'src/pconv_ops.cu',
+]
+
+setup(
+    name='PCFcuda',
+    version='1.1',
+    author='Logeswaran Sivakumar, Stefan Lee,  Pritesh Verma, Skand Peri, Li Fuxin',
+    author_email='loges.siva14@gmail.com',
+    description='PointConvFormer CUDA Kernel',
+    ext_modules=[
+        CUDAExtension('pcf_cuda', src_files,
+        include_dirs=[
+            os.path.join(project_root, 'cpp_wrappers/cpp_pcf_kernel/include'),
+            os.path.join(project_root, 'cutlass/include'),
+            os.path.join(project_root, 'cutlass/tools/util/include'),
+            ],
+        extra_compile_args={'nvcc': ['-L/usr/local/cuda/lib64 -lcudadevrt -lcudart']})
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    })
